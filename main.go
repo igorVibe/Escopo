32b696b933712d0735a107230b8fff4b8f056bcc
SilvaVibe <igor.juan108@gmail.com>
2025-04-24 15:56:16 -0300

    Escopo

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+	fmt.Println(soma(a, b))
+	fmt.Println(subtrair(a, b))
+	fmt.Println(multiplicacao(a, b))
+	fmt.Println(divisao(a, b))
+}
